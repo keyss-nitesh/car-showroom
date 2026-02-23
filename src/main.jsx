bf8efe8f5f3222e94e8da40518ee98469c8cbe41
keyss-nitesh <Nitesh@keyss.in>
2026-02-23 11:11:02 +0530

    fix: store path error for cloudflare

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import { store } from "./redux/store.jsx";
-import Api from "./Api.jsx";
-import Home from "./Home.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
